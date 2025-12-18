135d4eeff0c7d9754f8fb8a31de6c6fb6e6addb7
nitskii <nikita.omelnitskii@gmail.com>
2025-12-18 22:26:01 +0000

    add readiness endpoint & move static files to "app" directory

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { type Response } from 'express';
 
 const app = express();
 
-app.use(express.static('.'));
+app.use('/app', express.static('app'));
+
+app.get('/healthz', (_, res: Response) => {
+  res.set('content-type', 'text/plain; charset=utf-8');
+  res.send('OK');
+});
 
 const port = parseInt(process.argv[2]) || 8080;
 
