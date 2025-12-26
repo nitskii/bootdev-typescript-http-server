f8bc49481c122c7dd252c23553a4ea3db01c69db
nitskii <nikita.omelnitskii@gmail.com>
2025-12-26 12:18:09 +0000

    add user creation query

---
+++ src/db/queries.ts
@@ -0,0 +1,12 @@
+import { db } from 'src/db';
+import { NewUser, users } from 'src/db/schema';
+
+export const createUser = async (user: NewUser) => {
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning();
+
+  return result;
+}
